4f55bb6e06589da7b3eb2efb2c1e9d9c02be202b
Sandra <delovska.sandra@gmail.com>
2014-09-09 23:38:32 +0200

    Otvaranje na MainActivity :cake:
    Osnovnata strana so glavnoto meni

--- gen/com/example/animalia/R.java
+++ gen/com/example/animalia/R.java
@@ -1091,10 +1091,15 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003f;
+        public static final int action_settings=0x7f050044;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int btnAchievements=0x7f050043;
+        public static final int btnHighScores=0x7f050042;
+        public static final int btnLearn=0x7f05003f;
+        public static final int btnQuiz=0x7f050040;
+        public static final int btnSettings=0x7f050041;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1195,9 +1200,10 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
+        public static final int animals_layout=0x7f030018;
         public static final int list_item=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int main_layout=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
